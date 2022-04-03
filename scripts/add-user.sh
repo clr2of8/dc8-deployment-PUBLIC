04b1d2ce62c29ea1ec72971c0cac66cdf2236687
Carrie Roberts <clr2of8@gmail.com>
2022-04-02 21:04:17 -0600

    Update add-user.sh

--- scripts/add-user.sh
+++ scripts/add-user.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "****Adding the 'art' user****"
-sudo su -c "useradd artZZ -s /bin/bash -m"
+sudo su -c "useradd art -s /bin/bash -m"
 sudo chpasswd << 'END'
 art:AtomicRedTeam1!
 END
