c5f4ee01e274dfe4f2bea46bac3c983db97077ae
Carrie Roberts <clr2of8@gmail.com>
2022-04-02 20:18:06 -0600

    Update set-ip.sh

--- scripts/set-ip.sh
+++ scripts/set-ip.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
-ip=`curl ipconfig.io`
+ip=$(ip route get 8.8.8.8 | awk -F"src " 'NR==1{split($2,a," ");print a[1]}')
 sed -i -r "s/VECTR_HOSTNAME\=.*$/VECTR_HOSTNAME=$ip/g" /opt/vectr/.env
+docker-compose down
+docker-compose up -d
