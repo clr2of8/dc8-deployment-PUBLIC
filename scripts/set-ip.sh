a849bdf246bc17e2b35e57f852b181276e0132ac
Carrie Roberts <clr2of8@gmail.com>
2022-04-19 19:22:16 -0600

    Update set-ip.sh

--- scripts/set-ip.sh
+++ scripts/set-ip.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 ip=$(ip route get 8.8.8.8 | awk -F"src " 'NR==1{split($2,a," ");print a[1]}')
-sed -i -r "s/VECTR_HOSTNAME\=.*$/VECTR_HOSTNAME=$ip/g" /opt/vectr/.env
+hostname=$(hostname)
+sed -i -r "s/VECTR_HOSTNAME\=.*$/VECTR_HOSTNAME=$hostname/g" /opt/vectr/.env
 cd /opt/vectr
 docker-compose down
 docker-compose up -d
